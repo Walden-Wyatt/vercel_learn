ea9e7dbb18493b2190bc18cfe6770580ef53049f
unknown <investob848@gmail.com>
2024-05-03 04:39:21 -0700

    added post route

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.get("/home", (req, res) => {
     res.status(200).send("This is home page")
 });
 
+app.post("/login", (req, res) => {
+    res.status(201).send("Login successful !")
+})
+
 app.listen(4444, () => {
     console.log("Server is Running on PORT 4444");
 });
