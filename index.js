1546efb072ab4516db1f9cd8c0e7743b3659fff8
unknown <investob848@gmail.com>
2024-05-03 04:37:17 -0700

    first commit

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ app.use(e.json());
 
 app.get('/', (req, res) => {
     res.status(200).send("Hello from the Vercel !");
-})
+});
+
+app.get("/home", (req, res) => {
+    res.status(200).send("This is home page")
+});
 
 app.listen(4444, () => {
     console.log("Server is Running on PORT 4444");
